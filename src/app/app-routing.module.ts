b1ac9bbceb653c9bcad08e4fb9fc6f3395cd2f0a
jpjangid <developerjp2000@gmail.com>
2022-04-08 02:14:10 -0200

    created home page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { IndexComponent } from './index/index.component';
+
+const routes: Routes = [
+  // {path: '',   redirectTo: '/index', pathMatch: 'full'},
+  // {path: 'index', component: IndexComponent},
+  {path:'contact', component:ContactUsComponent, pathMatch:"full"}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'primeng/carousel';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { IndexComponent } from './index/index.component';
+import { RouterModule, Routes } from '@angular/router';
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarouselComponent,
+    TestimonialsComponent,
+    ContactUsComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    ToastModule,
+    ButtonModule
+  ],
+
+  exports: [
+    CarouselComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
